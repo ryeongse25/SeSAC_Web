ad5b741344f93e91798a2d97f4a5dafa76d46db3
ryeongse25 <seryeong971120@gmail.com>
2022-07-27 17:07:44 +0900

    study: practice

--- nodeJS/220727/practice33.js
+++ nodeJS/220727/practice33.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const bodyParser = require("body-parser");
+const fs = require("fs").promises;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -12,9 +13,35 @@ app.get("/", function(req, res) {
     res.render("practice33");
 })
 
+app.get("/login", function(req, res) {
+    res.render("login");
+})
+
 app.get("/receive", function(req, res) {
     console.log(req.query);
     res.send(req.query);
+})
+
+app.post("/receive", function(req, res) {
+    console.log(req.body);
+
+    let id = req.body.name;
+    let pw = req.body.password;
+
+    fs.readFile("info.txt")
+    .then(function(data) {
+
+        data = data.toString().split("//");
+
+        let right_id = data[0];
+        let right_pw = data[1];
+
+        if (id == right_id && pw == right_pw) {
+            res.send(true);
+        } else {
+            res.send(false);
+        }
+    })
 })
 
 app.listen(port, () => {
