0dda0eb324f5552f668bba4700ff212d079733b2
ryeongse25 <seryeong971120@gmail.com>
2022-07-27 18:33:41 +0900

    study: fetch & ajax

---
+++ nodeJS/220727/ajax.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const bodyParser = require("body-parser");
+const fs = require("fs").promises;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+    res.render("ajax");
+})
+
+app.get("/login", function(req, res) {
+    res.render("ajax_login");
+})
+
+
+// 회원가입
+app.get("/receive", function(req, res) {
+    console.log(req.query);
+    res.send(req.query);
+})
+
+// 로그인
+app.post("/receive", function(req, res) {
+    console.log(req.body);
+
+    let id = req.body.name;
+    let pw = req.body.password;
+
+    fs.readFile("info.txt")
+    .then(function(data) {
+
+        data = data.toString().split("//");
+
+        let right_id = data[0];
+        let right_pw = data[1];
+
+        if (id == right_id && pw == right_pw) {
+            res.send(true);
+        } else {
+            res.send(false);
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log("Server Port : ", port);
+})
