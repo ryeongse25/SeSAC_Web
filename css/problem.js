4fa5ff35b784844a1385b6ca982921fafcc1dc72
ryeongse25 <seryeong971120@gmail.com>
2022-09-05 18:48:20 +0900

    study: navigation bar

--- css/problem.js
+++ css/problem.js
@@ -1,5 +1,13 @@
 // js 동작 채우기
 
 function showMenu() {
-    $(".slide_menu").toggleClass("d-none");
+    if ( $(".slide_menu").hasClass("slide_menu_item") ) {
+        $(".slide_menu").removeClass("hideAnimation");
+        $(".slide_menu").removeClass("slide_menu_item");
+        $(".slide_menu").addClass("showAnimation");
+    } else {
+        $(".slide_menu").removeClass("showAnimation");
+        $(".slide_menu").addClass("hideAnimation");
+        $(".slide_menu").addClass("slide_menu_item");
+    }
 }
