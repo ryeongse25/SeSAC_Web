7343f9ed69ab71be6bc79b5760bac3110b5c2f34
ryeongse25 <seryeong971120@gmail.com>
2022-09-15 17:56:12 +0900

    study: clone-slack

--- clone_slack/public/textbox.js
+++ clone_slack/public/textbox.js
@@ -1,3 +1,13 @@
-window.onload = function() {
-    console.log(window.innerWidth);
-}
+// window.onload = function() {
+//     if ( window.innnerWidth > 500 ) {
+//         const right_container = document.querySelector(".right_container");
+//         $(right_container).css("width", (window.innerWidth - 200) + "px");
+//     }
+// }
+
+// window.onresize = function() {
+//     if (window.innerWidth > 500) {
+//         const right_container = document.querySelector(".right_container");
+//         $(right_container).css("width", (window.innerWidth - 200) + "px");
+//     }
+// }
