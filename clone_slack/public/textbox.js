5b5dd5d56d26656b27203c35792a994f6b5ca215
ryeongse25 <seryeong971120@gmail.com>
2022-09-15 21:09:56 +0900

    study: clone-slack

--- clone_slack/public/textbox.js
+++ clone_slack/public/textbox.js
@@ -1,13 +1,29 @@
-// window.onload = function() {
-//     if ( window.innnerWidth > 500 ) {
-//         const right_container = document.querySelector(".right_container");
-//         $(right_container).css("width", (window.innerWidth - 200) + "px");
-//     }
-// }
+window.onload = function() {
+    const textbox_container = document.querySelector(".textbox_container");
+    const right_container = document.querySelector(".right_container");
+    const msgbox_container = document.querySelector(".msg_box");
 
-// window.onresize = function() {
-//     if (window.innerWidth > 500) {
-//         const right_container = document.querySelector(".right_container");
-//         $(right_container).css("width", (window.innerWidth - 200) + "px");
-//     }
-// }
+    textbox_container.removeAttribute('style');
+
+    $(msgbox_container).css("height", (right_container.offsetHeight - textbox_container.offsetHeight - 125) + "px");
+
+    if ( window.innnerWidth > 500 ) {
+        $(textbox_container).css("width", (window.innerWidth - 200 - 30) + "px");
+    } else {
+        $(textbox_container).css("width", (window.innerWidth - 30) + "px");
+    }
+}
+
+window.onresize = function() {
+    const textbox_container = document.querySelector(".textbox_container");
+    const right_container = document.querySelector(".right_container");
+    const msgbox_container = document.querySelector(".msg_box");
+
+    $(msgbox_container).css("height", (right_container.offsetHeight - textbox_container.offsetHeight - 125) + "px");
+
+    if (window.innerWidth > 500) {
+        $(textbox_container).css("width", (window.innerWidth - 200 - 30) + "px");        
+    } else {
+        $(textbox_container).css("width", (window.innerWidth - 30) + "px");
+    }
+}
