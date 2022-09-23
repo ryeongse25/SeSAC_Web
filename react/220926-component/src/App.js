09e7d9eb829978af5a535e75320100b8d90f1f46
ryeongse25 <seryeong971120@gmail.com>
2022-09-23 18:01:26 +0900

    study: component

---
+++ react/220926-component/src/App.js
@@ -0,0 +1,11 @@
+import Sample from './Sample';
+
+function App() {
+    return (
+        <div>
+            <Sample />
+        </div>
+    );
+}
+
+export default App;
---
+++ react/220926-component/src/Sample.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+
+// const Sample = () => {
+//     const fashion = ["청바지", "패딩", "반팔", "운동화"];
+//     const fashionList = fashion.map((fashion, index) => <li key={index}>{fashion}</li>)
+//     return(
+//         <ul>{fashionList}</ul>
+//     )
+// }
+
+const Sample = () => {
+
+    const [names, setNames] = useState([
+        {id: 1, text: '청바지'},
+        {id: 2, text: '패딩'},
+        {id: 3, text: '반팔'},
+        {id: 4, text: '운동화'}
+    ]);
+
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);
+
+    const onChange = (e) => {
+        setInputText(e.target.value);
+    }
+
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText("");
+    }
+
+    const onRemove = (id) => {
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+    }
+
+    const namesList = names.map(name => <li key={name.id} onDoubleClick={() => onRemove(name.id)}>{name.text}</li>);
+
+    return (
+        <div>
+            <input value={inputText} onChange={onChange}></input>
+            <button onClick={onClick}>추가</button>
+            <ul>
+                {namesList}
+            </ul>
+        </div>
+    )
+}
+
+export default Sample;
