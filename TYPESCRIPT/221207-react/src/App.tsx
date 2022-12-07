ebe89d4b1f4f5fb52f807b6cfc8a812444659a2b
ryeongse25 <seryeong971120@gmail.com>
2022-12-07 15:42:33 +0900

    study: react-typescript

---
+++ TYPESCRIPT/221207-react/src/App.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+type AppProps = {
+    name: string;
+    age?: number;
+}
+
+function App(props: AppProps) {
+    const [list, setList] = useState<number[]>([]);
+    return (
+        <div className="App">
+            {props.name}
+        </div>
+    );
+}
+
+export default App;
